75475679d092e0c421f8160a750bddbbd943b0ff
K Sai Kishan <sonusaikishan@gmail.com>
2019-08-24 14:56:41 +0530

    Completed Graphs

--- web/src/reducers/stats.js
+++ web/src/reducers/stats.js
@@ -4,28 +4,28 @@ const initialState = {
   totalCarbonReduced: 29.6,
   totalMoneySaved: 240,
   earthPointsHistory: [
-    { x: 1, y: 2 },
-    { x: 2, y: 3 },
-    { x: 3, y: 5 },
-    { x: 4, y: 4 }
+    { x: 1, y: 460 },
+    { x: 2, y: 780 },
+    { x: 3, y: 210 },
+    { x: 4, y: 550 }
   ],
   kmTravelledHistory: [
-    { x: 1, y: 2 },
-    { x: 2, y: 3 },
-    { x: 3, y: 5 },
-    { x: 4, y: 4 }
+    { x: 1, y: 4.9 },
+    { x: 2, y: 8.2 },
+    { x: 3, y: 3.1 },
+    { x: 4, y: 5.2 }
   ],
   moneySavedHistory: [
-    { x: 1, y: 2 },
-    { x: 2, y: 3 },
-    { x: 3, y: 5 },
-    { x: 4, y: 4 }
+    { x: 1, y: 40 },
+    { x: 2, y: 70 },
+    { x: 3, y: 20 },
+    { x: 4, y: 40 }
   ],
   carbonReducedHistory: [
-    { x: 1, y: 2 },
-    { x: 2, y: 3 },
-    { x: 3, y: 5 },
-    { x: 4, y: 4 }
+    { x: 1, y: 2.1 },
+    { x: 2, y: 5.8 },
+    { x: 3, y: 1.4 },
+    { x: 4, y: 2.4 }
   ]
 };
 
