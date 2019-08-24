63ef4de92af7c67cc44043e6dc4187db7b5afbf4
K Sai Kishan <sonusaikishan@gmail.com>
2019-08-24 13:36:09 +0530

    Changes

--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import userReducer from "./user";
+import statsReducer from "./stats";
 
 const rootReducer = combineReducers({
-  user: userReducer
+  user: userReducer,
+  stats: statsReducer
 });
 
 export default rootReducer;
